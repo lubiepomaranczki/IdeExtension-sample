d590e4eaa9d62e695b537c2d5b4465a6e142df80
Łukasz Ławicki <lla@trackman.com>
2018-11-20 19:43:48 +0100

    Added working sample from Medium

---
+++ ExtensionSample/ExtensionSample/Templates/FunPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using Async = System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ${Namespace }
+{
+    public class ${EscapedIdentifier}Model
+    {
+        public ICommand ViewContentCommand { get; }
+
+        public ${EscapedIdentifier}Model()
+        {
+
+        }
+    }    
+}
