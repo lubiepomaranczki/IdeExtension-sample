25dfa7211917c5f75aec6e36bd02bfbbd01e7527
Łukasz Ławicki <lla@trackman.com>
2018-12-04 10:12:30 +0100

    Added MvvmCrossConverter

---
+++ ExtensionSample/ExtensionSample/MvvmCross/Converter/MvvmCrossConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace ${Namespace}
+{
+    //TODO Change object-object to your types
+    public class ${EscapedIdentifier} : MvxValueConverter<object,object>
+    {
+        //TODO Change returnt type and parameter type to match the above
+        protected override object Convert(object value,Type targetType, object parameter, CultureInfo culture)
+        {
+            throw NotImplementedException();
+        }
+    }
+}
